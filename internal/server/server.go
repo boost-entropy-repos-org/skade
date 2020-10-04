6f7796694e77f1c5ff6f04b727958ccc3c0d5a8a
Mindslave <mindslave.blog@protonmail.com>
2020-10-04 18:52:11 +0200

    added bulma css

--- internal/server/server.go
+++ internal/server/server.go
@@ -21,6 +21,7 @@ func (s *Server) routes() {
 	s.HandleFunc("/", s.indexPage()).Methods("GET")
 	s.HandleFunc("/login", s.loginPage()).Methods("GET")
 	s.HandleFunc("/home", s.homePage()).Methods("GET")
+	s.HandleFunc("/static/css/bulma.min.css", s.css()).Methods("GET")
 }
 
 func (s *Server) indexPage() http.HandlerFunc {
@@ -53,6 +54,27 @@ func (s *Server) homePage() http.HandlerFunc {
 		files := []string{
 			filepath.Join(wd + "/web/templates/test.html"),
 			filepath.Join(wd + "/web/templates/base.html"),
+		}
+		t, err := template.ParseFiles(files...)
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, p)
+	}
+}
+
+func (s *Server) css() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/css")
+		wd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		p := Page{
+			Title: "Index",
+		}
+		files := []string{
+			filepath.Join(wd + "/web/static/css/bulma.min.css"),
 		}
 		t, err := template.ParseFiles(files...)
 		if err != nil {
