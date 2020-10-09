b1ff1fbdaff3128ddf1ba502a9a9bfea75de0843
Mindslave <mindslave.blog@protonmail.com>
2020-10-09 21:16:19 +0200

    working on pe file analysis

--- internal/engine/pefile.go
+++ internal/engine/pefile.go
@@ -1,14 +1,16 @@
 package engine
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func isPeFile() (bool, error) {
-	dosStub, err := checkDosStub()
+	MZ, err := check_MZ_signatur()
 	if err != nil {
 		return false, err
 	}
-	//if no DOS Stub is detected, this is not a PE file
-	if dosStub != true {
+	//if no MZ Signature is detected, this is not a PE file
+	if MZ != true {
 		return false, nil
 	}
 	return true, nil
@@ -18,5 +20,21 @@ func isPeFile() (bool, error) {
 func checkDosStub() (bool, error) {
 	stubbytes := bytes[0:8]
 	fmt.Println(stubbytes)
+	return true, nil
+}
+
+// Every Windows executeable has to start with MZ for the first 2 bytes
+// Thus, if this file does not start with MZ it cannot be a pe file
+func check_MZ_signatur() (bool, error) {
+	MZ_bytes := bytes[0:2]
+	fmt.Println(MZ_bytes)
+	// 77 Decimal Ascii representation of M
+	if MZ_bytes[0] != 77 {
+		return false, nil
+	}
+	// 90 Decimal Ascii representation of M
+	if MZ_bytes[1] != 90 {
+		return false, nil
+	}
 	return true, nil
 }
