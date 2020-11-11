0f537c12f1bb4778233f3ad710cfbc2b6f096a51
Mindslave <mindslave.blog@protonmail.com>
2020-11-11 16:36:21 +0100

    building migrations

---
+++ db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "users" (
+      "id" bigserial PRIMARY KEY,
+      "username" varchar NOT NULL,
+      "email" email NOT NULL,
+      "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "files" (
+      "id" bigserial PRIMARY KEY,
+      "filename" varchar NOT NULL,
+      "filesize" int NOT NULL,
+      "fileextension" varchar,
+      "uploaded_at" timestamp DEFAULT (now()),
+      "uploaded_by" varchar
+);
+
+CREATE TABLE "Reports" (
+      "id" bigserial PRIMARY KEY,
+      "file" bigint,
+      "malicious" boolean NOT NULL
+);
+
+ALTER TABLE "files" ADD FOREIGN KEY ("uploaded_by") REFERENCES "users" ("id");
+
+ALTER TABLE "Reports" ADD FOREIGN KEY ("file") REFERENCES "files" ("id");
--- pkg/pefiles/pe.go
+++
@@ -1,9 +0,0 @@
-package pefiles
-
-func NewPEFile(fileName string) (pefile *PEFile, err error) {
-	pefile = new(PEFile)
-	pefile.FileName = fileName
-
-	// end of the happy path
-	return pefile, nil
-}
