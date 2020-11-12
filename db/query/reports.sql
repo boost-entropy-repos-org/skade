c56595dbf98419a6e4c85c6ea0a8f584afd5168d
Mindslave <mindslave.blog@protonmail.com>
2020-11-12 15:50:09 +0100

    added reports.sql

---
+++ db/query/reports.sql
@@ -0,0 +1,17 @@
+
+-- name: CreateReport :one
+INSERT INTO reports (
+    fileid,
+    malicious
+) VALUES (
+    $1, $2
+) RETURNING *;
+
+
+-- name: GetReport :one
+SELECT * FROM reports
+WHERE id = $1 LIMIT 1;
+
+-- name: ListReports :many
+SELECT * FROM reports
+ORDER BY name;
