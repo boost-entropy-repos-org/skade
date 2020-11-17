2691745e7fdc0ee85ad2a264cf20f5cbcce64e29
Mindslave <mindslave.blog@protonmail.com>
2020-11-17 20:07:27 +0100

    build repo object to work with the database

---
+++ db/sqlc/repo.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+//Repo provides all functions to execute database queries and transactions
+type Repo struct {
+	*Queries
+	db *sql.DB
+}
+
+//NewRepo ceates a new Repo
+func NewRepo(db *sql.DB) *Repo {
+	return &Repo {
+		db:		db,
+		Queries: New(db),
+	}
+}
+
+//execTX executes an entire transaction in the database
+func (repo *Repo) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := repo.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil  {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
