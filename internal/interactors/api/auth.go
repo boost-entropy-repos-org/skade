a9cd9af2bb0b2daac459f26b15df407e1424690c
Mindslave <mindslave.blog@protonmail.com>
2020-11-22 10:46:05 +0100

    building jwt auth

--- internal/interactors/api/auth.go
+++ internal/interactors/api/auth.go
@@ -7,7 +7,10 @@ import (
 )
 
 func (s *APIServer) authMiddleware(ctx *gin.Context) {
-	s.logger.Debug("auth")
-	fmt.Fprintf(ctx.Writer, "Not Authorized")
-	ctx.Abort()
+	s.logger.Debug("authorizing request")
+	if ctx.Request.Header["Token"] == nil {
+		s.logger.Debug("authorization failed")
+		fmt.Fprintf(ctx.Writer, "Not Authorized")
+		ctx.Abort()
+	}
 }
