ffd974d8359fc392d98ca7da9b5b90e01e7e2607
Mindslave <mindslave.blog@protonmail.com>
2020-11-27 15:58:13 +0100

    working on api

--- internal/interactors/api/upload.go
+++ internal/interactors/api/upload.go
@@ -3,12 +3,19 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (api *APIServer) upload(ctx *gin.Context) {
 	file, err := ctx.FormFile("file")
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, fmt.Errorf("Failed to get the file: %w", err))
+		return
+	}
+	filename := filepath.Base(file.Filename)
+	err = ctx.SaveUploadedFile(file, filename)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, fmt.Errorf("Failed to get the file: %w", err))
 		return
