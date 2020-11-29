eb7cfc1614a96e52c97a5022a04baefd5567c8a0
Mindslave <mindslave.blog@protonmail.com>
2020-11-29 15:26:20 +0100

    fixing file upload

--- internal/repositories/postgresql/file.go
+++ internal/repositories/postgresql/file.go
@@ -18,14 +18,13 @@ func (r* Repo) StoreFile(ctx context.Context, arg entities.DbStoreFileParams) (e
 	if err != nil {
 		return entities.File{}, fmt.Errorf("error generating uuid: %w", err)
 	}
-	err = r.Get(&u, "INSERT INTO files VALUES($1, $2, $3, $4) RETURNING *", 
+	err = r.Get(&file, "INSERT INTO files VALUES($1, $2, $3, $4) RETURNING *", 
 				uuid,
 				arg.Filename,
 				arg.Filesize,
 				time.Now())
 	if err != nil {
-		return entities.User{}, fmt.Errorf("error creating user: %w", err)
+		return entities.File{}, fmt.Errorf("error creating user: %w", err)
 	}
 	return file, nil
-}
 }
