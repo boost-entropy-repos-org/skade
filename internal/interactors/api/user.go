81e769bfe0452aebb6c5d396d0f3baa1279fbf4c
Mindslave <mindslave.blog@protonmail.com>
2020-11-30 20:15:01 +0100

    adding pw to user api

--- internal/interactors/api/user.go
+++ internal/interactors/api/user.go
@@ -24,6 +24,7 @@ func (s *APIServer) createUser(ctx *gin.Context) {
 
 	arg := entities.DbCreateUserParams {
 		Username: req.Username,
+		HashedPassword: req.HashedPassword,
 		Email: req.Email,
 	}
 
