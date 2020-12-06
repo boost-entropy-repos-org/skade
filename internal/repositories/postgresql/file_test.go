1c8ca70297f76c021837406097fe0360b043dd0f
Mindslave <marius.kimmina@protonmail.com>
2020-12-06 15:51:29 +0100

    testing file storage

--- internal/repositories/postgresql/file_test.go
+++ internal/repositories/postgresql/file_test.go
@@ -2,5 +2,19 @@ package postgresql
 
 import(
 	"testing"	
+	"context"
+
+	"github.com/Mindslave/skade/internal/entities"
+
+	"github.com/stretchr/testify/require"
 )
 
+func TestStoreFile(t *testing.T) {
+	arg := entities.DbStoreFileParams {
+		Filename: "testfile",
+		Filesize: 100,
+	}
+
+	err := testrepo.StoreFile(context.Background(), arg)
+	require.NoError(t, err)
+}
