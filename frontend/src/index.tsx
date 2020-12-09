d3fdfa6b9ca58573fc37b314138b45f12569dd7a
Mindslave <mindslave.blog@protonmail.com>
2020-12-09 19:51:34 +0100

    start building frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import HomePage from './pages/home';
+import AdminPage from './pages/admin';
+
+function App() {
+  return (
+    <div className="App container">
+      <div className="jumbotron">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/admin" component={AdminPage}/>
+          </Switch>
+        </Router>
+      </div>
+    </div>
+  );
+}
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App/>, rootElement);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
